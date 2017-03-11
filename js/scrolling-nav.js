9c13899602e1165c154e289fad8bf5c01bf61071
Omar Al-Masry <omar.almasry91@gmail.com>
2017-03-11 12:23:34 +0200

    Update scrolling-nav.js

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -1,20 +1,19 @@
-$(document).ready(function() {
-  $('body').hide();
-   $('body').fadeIn(1000);    
+$(document).ready(function () {
+    $('body').hide();
+    $('body').fadeIn(1000);
 });
-
 //jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
+$(window).scroll(function () {
     if ($(".navbar").offset().top > 50) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
+    }
+    else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
     }
 });
-
 //jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
+$(function () {
+    $('a.page-scroll').bind('click', function (event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
